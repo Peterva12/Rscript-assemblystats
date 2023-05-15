fd9b6010379cc0983f51eb3f1da150c5e7b69704
Peterva12 <133684197+Peterva12@users.noreply.github.com>
2023-05-16 02:13:28 +0930

    Update easy_read_assesmbly-stats.R

--- easy_read_assesmbly-stats.R
+++ easy_read_assesmbly-stats.R
@@ -1,20 +1,21 @@
+# Load the stringr package
 library(stringr)
 
-# Specify the path to the assembly-stats executable for this example I have it in the same directory as the script
+# Specify the path to the assembly-stats executable
 assemblyStatsPath <- "./assembly-stats"
 
-# Specify the path to the dataset file
-lrDatasetFile <- "~/flye_assembly_output/pacbio_LR_30x_assembly/assembly.fasta"
+# Specify the path to the LR dataset file
+lrDatasetFile <- "~/Assignment2/flye_assembly_output/pacbio_LR_30x_assembly/assembly.fasta"
 
-# Defining a function to run assembly-stats
+# Define a function to run assembly-stats and extract information
 runAssemblyStats <- function(assemblyStatsPath, lrDatasetFile) {
-  # This builds the command to run assembly-stats
+  # Build the command to run assembly-stats
   command <- paste(assemblyStatsPath, lrDatasetFile, sep=" ")
   
   # Run assembly-stats and capture the output
   output <- system(command, intern=TRUE)
   
-  # This extracts the desired information from the output of assembly-stats
+  # Extract the desired information from the output
   totalBases <- as.numeric(str_extract(output[2], "(?<=sum\\s=\\s)\\d+"))
   numReads <- as.numeric(str_extract(output[2], "(?<=sum\\s=\\s)\\d+"))
   avgReadLength <- as.numeric(str_extract(output[2], "(?<=ave\\s=\\s)\\d+"))
@@ -23,8 +24,15 @@ runAssemblyStats <- function(assemblyStatsPath, lrDatasetFile) {
   numContigs <- as.numeric(str_extract(output[2], "(?<=n\\s=\\s)\\d+"))
   largestContigLength <- as.numeric(str_extract(output[2], "(?<=largest\\s=\\s)\\d+"))
   nFifty <- as.numeric(str_extract(output[3], "(?<=N50\\s=\\s)\\d+"))
+  nSixty <- as.numeric(str_extract(output[4], "(?<=N60\\s=\\s)\\d+"))
+  nSeventy <- as.numeric(str_extract(output[5], "(?<=N70\\s=\\s)\\d+"))
+  nEighty <- as.numeric(str_extract(output[6], "(?<=N80\\s=\\s)\\d+"))
+  nNinety <- as.numeric(str_extract(output[7], "(?<=N90\\s=\\s)\\d+"))
+  nHundred <- as.numeric(str_extract(output[8], "(?<=N100\\s=\\s)\\d+"))
+  nCount <- as.numeric(str_extract(output[9], "(?<=N_count\\s=\\s)\\d+"))
+  gapsAssem <- as.numeric(str_extract(output[10], "(?<=Gaps\\s=\\s)\\d+"))
     
-  # Makke the extracted info into a list
+  # Return the extracted information as a list
   return(list(
     totalBases = totalBases,
     numReads = numReads,
@@ -33,9 +41,33 @@ runAssemblyStats <- function(assemblyStatsPath, lrDatasetFile) {
     draftGenomeSize = draftGenomeSize,
     numContigs = numContigs,
     largestContigLength = largestContigLength,
-    nFifty = nFifty
+    nFifty = nFifty,
+    nSixty = nSixty,
+    nSeventy = nSeventy,
+    nEighty = nEighty,
+    nNinety = nNinety,
+    nHundred = nHundred,
+    nCount = nCount,
+    gapsAssem = gapsAssem
   ))
 }
 
-# Run assembly-stats and output the extracted information for the dataset
+# Run assembly-stats and extract information for the LR dataset
 result <- runAssemblyStats(assemblyStatsPath, lrDatasetFile)
+
+# Print the extracted information
+cat("Total number of bases:", result$totalBases, "\n")
+cat("Number of reads:", result$numReads, "\n")
+cat("Average read length:", result$avgReadLength, "\n")
+cat("Largest read length:", result$largestReadLength, "\n")
+cat("Draft Genome Size:", result$draftGenomeSize, "\n")
+cat("number of contigs:", result$numContigs, "\n")
+cat("Largest Contig Length:", result$largestContigLength, "\n")
+cat("N50:", result$nFifty, "\n")
+cat("N60:", result$nSixty, "\n")
+cat("N70:", result$nSeventy, "\n")
+cat("N80:", result$nEighty, "\n")
+cat("N90:", result$nNinety, "\n")
+cat("N100:", result$nHundred, "\n")
+cat("N count:", result$nCount, "\n")
+cat("Gaps:", result$gapsAssem, "\n")
